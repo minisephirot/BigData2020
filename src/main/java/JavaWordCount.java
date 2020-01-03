76677ebf21388067cea7097fd492fd681b7bc4f6
minisephirot <minisephirot@hotmail.fr>
2020-01-03 15:44:26 +0100

    Installation lib ml pour les algo de mining FPGrowth

--- src/main/java/JavaWordCount.java
+++ src/main/java/JavaWordCount.java
@@ -27,52 +27,52 @@ import java.util.regex.Pattern;
 
 public final class JavaWordCount {
 
-  private static final Pattern SPACE = Pattern.compile(" +");
+    private static final Pattern SPACE = Pattern.compile(" +");
 
-  public static void main(String[] args){
+    public static void main(String[] args){
 
-    //Permet le fonctionnement de hadoop pour Windows
-    System.setProperty("hadoop.home.dir", "C:\\winutil\\");
+        //Permet le fonctionnement de hadoop pour Windows
+        System.setProperty("hadoop.home.dir", "C:\\winutil\\");
 
-    //Démarrage de Spark en Java
-    SparkSession spark = SparkSession
-            .builder()
-            .appName("JavaWordCount")
-            .config("spark.master", "local")
-            .getOrCreate();
+        //Démarrage de Spark en Java
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .config("spark.master", "local")
+                .getOrCreate();
 
-    //Etape 1 : Parsing des .txt et des stopwords en JavaRDD pour travailler dessus
-    JavaRDD<String> lines = spark.read().textFile("src/main/resources/cf/*").javaRDD(); //Pour l'instant sur un document, il suffit de changer en cf* ensuite
-    JavaRDD<String> stopwords = spark.read().textFile("src/main/resources/french-stopwords.txt").javaRDD();
-    JavaRDD<String> stopwordsMaj = stopwords.map(StringUtils::capitalize);
+        //Etape 1 : Parsing des .txt et des stopwords en JavaRDD pour travailler dessus
+        JavaRDD<String> lines = spark.read().textFile("src/main/resources/cf/*").javaRDD(); //Pour l'instant sur un document, il suffit de changer en cf* ensuite
+        JavaRDD<String> stopwords = spark.read().textFile("src/main/resources/french-stopwords.txt").javaRDD();
+        JavaRDD<String> stopwordsMaj = stopwords.map(StringUtils::capitalize);
 
-    //Etape 2: un mot par element, on sort les espaces, les lignes vides, puis la ponctuation
-    JavaRDD<String> words = lines.flatMap(data -> Arrays.asList(SPACE.split(data)).iterator());
+        //Etape 2: un mot par element, on sort les espaces, les lignes vides, puis la ponctuation
+        JavaRDD<String> words = lines.flatMap(data -> Arrays.asList(SPACE.split(data)).iterator());
 
-    words = words.filter(data -> !data.isEmpty());
-    words = words.filter(data -> !data.matches("\\W|\\d") );
+        words = words.filter(data -> !data.isEmpty());
+        words = words.filter(data -> !data.matches("\\W|\\d") );
 
-    //Etape 3 : Filtrage des stopwords (On le fais avant de finir l'étape 2 car plus rapide de travailler sur une liste de String que des Tuples)
-    long before = words.count();
-    words = words.subtract(stopwords);
-    words = words.subtract(stopwordsMaj);
-    System.out.println("Il y avait "+ before +" éléments, les stop words ont retirés "+ (before-words.count())+" mots." );
+        //Etape 3 : Filtrage des stopwords (On le fais avant de finir l'étape 2 car plus rapide de travailler sur une liste de String que des Tuples)
+        long before = words.count();
+        words = words.subtract(stopwords);
+        words = words.subtract(stopwordsMaj);
+        System.out.println("Il y avait "+ before +" éléments, les stop words ont retirés "+ (before-words.count())+" mots." );
 
-    //On associe une occurence a chaque mots pour compter ensuite
-    JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
-    ones.foreach(data -> System.out.println("Mot :" + data._1() + " = " + data._2()));
+        //On associe une occurence a chaque mots pour compter ensuite
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s, 1));
+        ones.foreach(data -> System.out.println("Mot :" + data._1() + " = " + data._2()));
 
-    //On somme les occurences sur les clés
-    JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
-    counts.foreach(data -> System.out.println("Mot :" + data._1() + " = " + data._2()));
+        //On somme les occurences sur les clés
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+        counts.foreach(data -> System.out.println("Mot :" + data._1() + " = " + data._2()));
 
-    //Etape 4 : Récupèrer les 10 premiers
-    JavaPairRDD<Integer, String> reversed = counts.mapToPair(t -> new Tuple2<>(t._2, t._1));
-    reversed = reversed.sortByKey(false);
-    List<Tuple2<Integer, String>> reversedList = reversed.take(10);
-    reversedList.forEach(data -> System.out.println("Mot : " + data._2 + " = " + data._1));
+        //Etape 4 : Récupèrer les 10 premiers
+        JavaPairRDD<Integer, String> reversed = counts.mapToPair(t -> new Tuple2<>(t._2, t._1));
+        reversed = reversed.sortByKey(false);
+        List<Tuple2<Integer, String>> reversedList = reversed.take(10);
+        reversedList.forEach(data -> System.out.println("Mot : " + data._2 + " = " + data._1));
 
-    spark.stop();
-  }
+        spark.stop();
+    }
 
 }
