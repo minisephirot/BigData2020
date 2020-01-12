9419f0ca66bcda63988f6f92fd906de2719371c2
minisephirot <minisephirot@hotmail.fr>
2020-01-12 17:13:06 +0100

    Fixing + Prep ReadMe

--- src/main/java/JavaWordMining.java
+++ src/main/java/JavaWordMining.java
@@ -14,15 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.apache.spark.ml.feature.StopWordsRemover;
 import org.apache.spark.ml.fpm.FPGrowth;
@@ -30,8 +21,22 @@ import org.apache.spark.ml.fpm.FPGrowthModel;
 import org.apache.spark.sql.*;
 import org.apache.spark.sql.types.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public final class JavaWordMining {
+
     private static final Pattern SPACE = Pattern.compile("\\s+");
+    private static final double minsup = 0.5;
+    private static final double minconf = 0.5;
 
     public static void main(String[] args) throws IOException {
 
@@ -44,65 +49,62 @@ public final class JavaWordMining {
                 .config("spark.master", "local")
                 .getOrCreate();
 
-        //Récupération du dossier contenant tous les fichiers
-        File folder = new File("src/main/resources/cf");
+        //Etape 1 : Parsing des .txt et des stopwords en transactions pour travailler dessus, on retirera aussi la ponctuation et les espaces vides
+        File cf = new File("src/main/resources/cf");
+        File cp = new File("src/main/resources/cp");
+
+        //Etape 2: Filtrage des stopwords dans chaques transactions
+        String load = new String(Files.readAllBytes(Paths.get("src/main/resources/french-stopwords.txt")));
+        //Transformation en tableau de string pour chaque mot
+        List<String> stopwords = Arrays.asList(SPACE.split(load));
+
+        //Classe faite pour retirer les stopwords
+        StopWordsRemover remover = new StopWordsRemover();
+        remover.setStopWords((String[]) stopwords.toArray());
+        //Définition de la colonne à modifier
+        remover.setInputCol("items");
+        //Définition de la colonne crée à la fin
+        remover.setOutputCol("new");
+
+        wordMining(cf,spark,remover);
+        //wordMining(cp,spark,remover);
+
+        spark.stop();
+    }
+
+    static void wordMining(File folder, SparkSession spark, StopWordsRemover remover) throws IOException {
 
         ArrayList<Row> tab = new ArrayList<>();
-
-        //Parcours de chaque fichier
-        for (File f: folder.listFiles()) {
-            //Transformation du fichier en string
+        for (File f: Objects.requireNonNull(folder.listFiles())) {
             String load = new String(Files.readAllBytes(Paths.get(f.getPath())));
-
             //Remplacement des caractère ne faisant pas partie des mots
-            load = load.replaceAll("[^\\/ éèàù’\\w]","");
-
+            load = load.replaceAll("[^/ éèàù’\\w]","").replaceAll("\\d","");
             //Ajout à la liste des transactions
-            tab.add(RowFactory.create(Arrays.asList(SPACE.split(load)).stream().distinct().collect(Collectors.toList()))) ;
+            tab.add(RowFactory.create(Arrays.stream(SPACE.split(load)).distinct().collect(Collectors.toList()))) ;
         }
-
-        //Transformation en string du fichier de stopwords
-        String load = new String(Files.readAllBytes(Paths.get("src/main/resources/french-stopwords.txt")));
-
-        //Transformation en tableau de string pour chaque mot
-        List<String> stop = Arrays.asList(SPACE.split(load));
 
         StructType schema = new StructType(new StructField[]{ new StructField(
                 "items", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
         });
-
         //Création du Dataset à partir du tableau de transaction et du schéma
         Dataset<Row> lines = spark.createDataFrame(tab, schema);
 
-        //Création du remover afin d'enlever les mots présent dans le fichier stopwords
-        StopWordsRemover remove = new StopWordsRemover();
-        //récupération des mots interdits
-        remove.setStopWords((String[]) stop.toArray());
-        //Récupération de la colonne à modifier
-        remove.setInputCol("items");
-        //Colonne créer à la fin
-        remove.setOutputCol("new");
-
         //Application du remover sur le Dataset
-        lines = remove.transform(lines);
+        lines = remover.transform(lines);
 
         //Drop de l'ancienne colonne
         lines = lines.drop("items");
 
-        //Application des algorithmes
+        //Application de FPGrowth
         FPGrowthModel model = new FPGrowth()
                 .setItemsCol("new")
-                .setMinSupport(0.5)
-                .setMinConfidence(0.2)
+                .setMinSupport(minsup)
+                .setMinConfidence(minconf)
                 .fit(lines);
 
-        // Display frequent itemsets.
+        //Affichage.
         model.freqItemsets().orderBy(functions.col("freq").desc()).show(false);
-
-        // Display generated association rules
         model.associationRules().orderBy(functions.col("confidence").desc()).show(false);
-
-        spark.stop();
     }
 
 }
